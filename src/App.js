464422e5b7f961e97f6a401ec6f473c55d95999e
ale <ale@ale.com>
2022-04-30 00:57:42 -0300

    create header navegation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Logo from './components/Logo';
+import NavPage from './components/NavPage';
 import Routes from './Routes/routes';
+import { Header } from './style/style';
 
 function App() {
   return (
     <div>
+      <Header>
+        <Logo />
+        <NavPage />
+      </Header>
       <Routes />
     </div>
   );
---
+++ src/style/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    background-color: #000;
+    height: 70px;
+`
