2270bf441937a2ec438be22a10446d48cc708e03
ale <ale@ale.com>
2022-05-01 13:29:47 -0300

    feat: render products filtered by input in the ListProducts

--- src/components/ListProducts/index.jsx
+++ src/components/ListProducts/index.jsx
@@ -1,23 +1,43 @@
 import { useContext } from "react";
 import { Context } from "../../Context/AuthContext";
+
+import NotFound from "../NotFound";
 import Product from "../Products";
 
-function ListProducts() {
-  const { store } = useContext(Context);
+function ListProducts({ isfilter }) {
+  const { store, filterProducts } = useContext(Context);
 
   return (
     <ul>
-      {store.map((product) => {
-        return (
-          <Product
-            key={product.id}
-            title={product.title}
-            image={product.image}
-            price={product.price}
-            description={product.description}
-          />
-        );
-      })}
+      {isfilter ? (
+        filterProducts.length === 0 ? (
+          <NotFound />
+        ) : (
+          filterProducts.map((product) => {
+            return (
+              <Product
+                key={product.id}
+                title={product.title}
+                image={product.image}
+                price={product.price}
+                description={product.description}
+              />
+            );
+          })
+        )
+      ) : (
+        store.map((product) => {
+          return (
+            <Product
+              key={product.id}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })
+      )}
     </ul>
   );
 }
